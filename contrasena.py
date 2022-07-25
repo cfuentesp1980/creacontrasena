c74d260d1d9baf307f963c409682b98912c35096
Claudio Fuentes <claudiofuentesperez@gmail.com>
2022-07-24 23:45:09 -0700

    Este es mi primer commit

---
+++ contrasena.py
@@ -0,0 +1,11 @@
+def run():
+    resultado = int(input('Ingrese un numero cualquiera: '))
+    suma = resultado * 2
+    print (f'El doble de su numero es: {suma}')
+
+
+
+
+
+if __name__ == '__main__':
+    run()
